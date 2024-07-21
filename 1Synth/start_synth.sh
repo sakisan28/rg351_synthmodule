cacdffd4ddd517f4bdb2f4fb23192ae1105772f8
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-07-21 20:48:55 +0900

    Update start_synth.sh for LaunchPad Mini

--- 1Synth/start_synth.sh
+++ 1Synth/start_synth.sh
@@ -5,5 +5,5 @@ fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -
 sleep 2
 
 fluid=$(aconnect -o -l | awk '/FLUID/{ print substr($2,1,length($2)-1);exit }')
-k=$(aconnect -i -l | awk '/^client/ && !/(System|Midi\ Through)/{ print substr($2,1,length($2)-1);exit }')
+k=$(aconnect -i -l | awk '/^client/ && !/(System|Midi\ Through)/{ print substr($2,1,length($2)-1) ($0~/Launchpad\ Mini/?":1":"");exit }')
 aconnect ${k} ${fluid}
