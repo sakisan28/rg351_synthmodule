7aeac3f207c4bd98cb04c71541c0c869f82b23c6
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-07-23 14:48:04 +0900

    FluidR3_GM as default soundfont

--- 1Synth/start_synth.sh
+++ 1Synth/start_synth.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -g 1.0 /usr/share/sounds/sf3/default-GM.sf3 &
+fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -g 1.0 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
+#fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -g 1.0 /usr/share/sounds/sf3/default-GM.sf3 &
 
-sleep 2
+sleep 7
 
 fluid=$(aconnect -o -l | awk '/FLUID/{ print substr($2,1,length($2)-1);exit }')
 k=$(aconnect -i -l | awk '/^client/ && !/(System|Midi\ Through)/{ print substr($2,1,length($2)-1) ($0~/Launchpad\ Mini/?":1":"");exit }')
