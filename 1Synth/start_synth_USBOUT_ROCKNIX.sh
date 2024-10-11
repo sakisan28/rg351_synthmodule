9c87bfc50e349bcfcd67ba24c35d307b6ba55abd
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-10-11 15:48:14 +0900

    support ROCKNIX

--- 1Synth/start_synth_USBOUT_ROCKNIX.sh
+++ 1Synth/start_synth_USBOUT_ROCKNIX.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-fluidsynth -s -i -a pulseaudio -m alsa_seq -f $(dirname $0)/fluidsettings_ROCKNIX.txt &
+USBDEV=$(pactl list sinks short|grep 'alsa_output\.usb'|awk '{print $1;exit}')
 
+fluidsynth -s -i -a pulseaudio -o audio.pulseaudio.device=hw:${USBDEV} -m alsa_seq -f $(dirname $0)/fluidsettings_ROCKNIX.txt &
 sleep 5
 
 fluid=$(aconnect -o -l | awk '/FLUID/{ print substr($2,1,length($2)-1);exit }')
