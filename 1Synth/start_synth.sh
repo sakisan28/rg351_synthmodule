a33e49bfd1f1e483f2b5ef17d683b95b3c53342b
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2024-10-11 11:11:31 +0900

    outer setting file

--- 1Synth/start_synth.sh
+++ 1Synth/start_synth.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 
-fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -g 1.0 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
-#fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -g 1.0 /usr/share/sounds/sf3/default-GM.sf3 &
-
-sleep 7
+fluidsynth -s -i -a alsa -o audio.alsa.device=hw:rockchiprk817co,0 -m alsa_seq -f $(dirname $0)/fluidsettings.txt &
+sleep 5
 
 fluid=$(aconnect -o -l | awk '/FLUID/{ print substr($2,1,length($2)-1);exit }')
 k=$(aconnect -i -l | awk '/^client/ && !/(System|Midi\ Through)/{ print substr($2,1,length($2)-1) ($0~/Launchpad\ Mini/?":1":"");exit }')
